70a8ab45b075454ff804a1d7d67b4007d7f6b7f7
Dito Cahya Pratama <ditocahyapratama717@gmail.com>
2020-10-15 19:35:34 +0700

    Add directory and util for get API

---
+++ blog-frontend/src/utils/APIUtils.ts
@@ -0,0 +1,43 @@
+import { API_BASE_URL, ACCESS_TOKEN } from '../constants';
+
+const request = (options: Object) => {
+    const headers = new Headers({
+        'Content-Type': 'application/json',
+    })
+    
+    if(localStorage.getItem(ACCESS_TOKEN)) {
+        headers.append('Authorization', 'Bearer ' + localStorage.getItem(ACCESS_TOKEN))
+    }
+
+    const defaults = {headers: headers};
+    options = Object.assign({}, defaults, options);
+
+    return fetch(options.url, options)
+    .then(response => 
+        response.json().then(json => {
+            if(!response.ok) {
+                return Promise.reject(json);
+            }
+            return json;
+        })
+    );
+};
+
+export function login(loginRequest:string) {
+    return request({
+        url: API_BASE_URL + "/auth/local",
+        method: 'POST',
+        body: JSON.stringify(loginRequest)
+    });
+}
+
+export function getCurrentUser() {
+    if(!localStorage.getItem(ACCESS_TOKEN)) {
+        return Promise.reject("No access token set.");
+    }
+
+    return request({
+        url: API_BASE_URL + "/users/me",
+        method: 'GET'
+    });
+}
